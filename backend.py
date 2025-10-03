4c73b449c301c92a86a90a5f04128b8c3748e432
OksanaKis <oksanakisetoja@gmail.com>
2025-10-03 15:02:49 -0700

    Temps changed to C, add try and except blocks to catching errors

--- backend.py
+++ backend.py
@@ -8,12 +8,10 @@ def get_data(place, forecast_days, kind=None):
     response = requests.get(url) 
     data = response.json() 
     filtered_data = data["list"] 
-    # print(filtered_data) 
     nr_values = 8 * forecast_days 
     filtered_data = filtered_data[:nr_values] 
-    # print(filtered_data)
     if kind == "Temperature": 
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data] 
+        filtered_data = [dict["main"]["temp"]/10 for dict in filtered_data] 
     if kind == "Sky": 
         filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data 
